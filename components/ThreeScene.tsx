49de6968f8117b8bf539815c14798127c8e3fa12
yash raj <yashraj24007@users.noreply.github.com>
2025-12-24 16:36:58 +0530

    Update ThreeScene component

--- components/ThreeScene.tsx
+++ components/ThreeScene.tsx
@@ -39,14 +39,14 @@ const ThreeScene: React.FC = () => {
   return (
     <div className="w-full h-screen absolute inset-0 -z-10 bg-black">
       <Canvas camera={{ position: [0, 0, 8], fov: 45 }}>
-        {/* Environment for glossy reflections */}
-        <Environment preset="night">
+        {/* Environment for glossy reflections - using background instead of preset to avoid external dependencies */}
+        <Environment background={false}>
             <Lightformer intensity={4} color="white" position={[0, 5, -9]} scale={[10, 50, 1]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
             <Lightformer intensity={2} color="purple" position={[-5, 1, -1]} scale={[10, 2, 1]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
             <Lightformer intensity={2} color="blue" position={[10, 1, 0]} scale={[20, 2, 1]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
         </Environment>
 
-        <ambientLight intensity={0.2} />
+        <ambientLight intensity={0.3} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} intensity={1} color="#00f3ff" />
         <pointLight position={[-10, -10, -10]} intensity={1} color="#bc13fe" />
 
